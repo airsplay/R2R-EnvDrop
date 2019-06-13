4c115853b6e53dd245f965e99d63579372d7ebdb
airsplay <airsplay@cs.unc.edu>
2019-06-13 16:14:00 -0400

    Update running files

--- run/agent.bash
+++ run/agent.bash
@@ -4,6 +4,9 @@ flag="--attn soft --train listener
       --angleFeatSize 128
       --feedback sample
       --mlWeight 0.2
-      --subout max --dropout 0.5 --optim rms --lr 1e-4 --iters 1000000 --maxAction 35"
+      --subout max --dropout 0.5 --optim rms --lr 1e-4 --iters 80000 --maxAction 35"
 mkdir -p snap/$name
-CUDA_VISIBLE_DEVICES=$1 unbuffer python r2r_src/train.py $flag --name $name | tee snap/$name/log
+CUDA_VISIBLE_DEVICES=$1 python r2r_src/train.py $flag --name $name 
+
+# Try this with file logging:
+# CUDA_VISIBLE_DEVICES=$1 unbuffer python r2r_src/train.py $flag --name $name | tee snap/$name/log
--- run/speaker.bash
+++ run/speaker.bash
@@ -1,6 +1,9 @@
 name=speaker
 flag="--attn soft --angleFeatSize 128
       --train speaker
-      --subout max --dropout 0.6 --optim adam --lr 1e-4 --iters 1000000 --maxAction 35"
+      --subout max --dropout 0.6 --optim adam --lr 1e-4 --iters 80000 --maxAction 35"
 mkdir -p snap/$name
-CUDA_VISIBLE_DEVICES=$1 unbuffer python r2r_src/train.py $flag --name $name | tee snap/$name/log
+CUDA_VISIBLE_DEVICES=$1 python r2r_src/train.py $flag --name $name 
+
+# Try this for file logging
+# CUDA_VISIBLE_DEVICES=$1 unbuffer python r2r_src/train.py $flag --name $name | tee snap/$name/log
